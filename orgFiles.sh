c1a798a565183cfb651d04d651e35c29528a9fe8
Arthur <arthur.4lves@gmail.com>
2014-07-21 16:43:19 -0300

    Permite organizar arquivos por tipo.

--- orgFiles.sh
+++ orgFiles.sh
@@ -3,29 +3,32 @@
 
 log_path="$HOME/log.txt" 
 arquivo=$(zenity --entry --text "Que tipo de arquivo deseja procurar?")
+caminho=$(zenity --file-selection --title "De onde deseja procurar?" --directory)
 
 if [ -n "$arquivo" ] ; then
 #Busca somente na pasta local do usuário, mas pode ser alterado
-	find $HOME -name "$arquivo" \
+	find "$caminho/" -name "$arquivo" \
 	    | tee >(zenity --progress --text "Procurando" --pulsate --auto-close)\
 	    >$log_path
 
-	echo $(zenity --info --text "Encontrado `awk 'END { print NR }' $log_path` ocorrências")
+	zenity --info --text "Encontrado `awk 'END { print NR }' $log_path` ocorrências"
 
 	pasta=$(zenity --file-selection --title "Pra onde deseja movê-los?" --directory)
 
 	if [ -n "$pasta" ] ; then
 		while read linha 
 		do 
-		   mv -f "$linha" $pasta
+		   mv -f "$linha" "$pasta"
 		done < $log_path
 		rm $log_path
-		echo $(zenity --notification --text "Movido com sucesso para $pasta" --window-icon="info" )
+		zenity --notification --text "Movido com sucesso para $pasta" --window-icon="info"
 	else
-		echo $(zenity --info --text "Cancelado pelo usuário")
+		zenity --info --text "Cancelado pelo usuário"
+		sleep 3		
 	fi
 fi
 
 unset arquivo
 unset pasta
 unset log_path
+exit 1
